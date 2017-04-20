07f37364e197205b17f77d5a5d56690de296a495
Kevin Bao <kbao@stuy.edu>
2017-04-20 07:39:10 -0400

    added temp Ticket

---
+++ Ticket.java
@@ -0,0 +1,14 @@
+public class Ticket {
+    public String descr;
+    private int _ID;
+    private int priority;
+    private String username;
+
+    public Ticket() { //subject to change
+	descr = "help";
+	_ID = (int)(Math.random()*1000);
+	priority
+	
+	    }
+}
+    
