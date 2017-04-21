ee5f729126888b60a9649b62b07e73f811d54895
kaybowwow <kbao@stuy.edu>
2017-04-20 20:25:09 -0400

    Ticket level up!

--- Ticket.java
+++ Ticket.java
@@ -1,14 +1,54 @@
+import java.util.Scanner;
+
 public class Ticket {
-    public String descr;
+
+    public String desc;
     private int _ID;
     private int priority;
     private String username;
 
     public Ticket() { //subject to change
-	descr = "help";
-	_ID = (int)(Math.random()*1000);
-	priority
-	
-	    }
+     desc = "help";
+     _ID = (int)(Math.random()*1000);
+     priority = 1;
+     username = "tracer";
+    }
+
+    public Ticket(String woes , String name) { //subject to change
+     desc = woes;
+     _ID = (int)(Math.random()*1000);
+     priority = 1;
+     username = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+    public int getID() {
+        return _ID;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getName() {
+        return username;
+    }
+
+    public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        //Scanner sd = new Scanner(System.in);
+        String input2 = "harddrive";
+
+        Ticket hero = new Ticket(input2 , input);
+
+        System.out.println(hero.getDesc());
+        System.out.println(hero.getID());
+        System.out.println(hero.getPriority());
+        System.out.println(hero.getName());
+    }
+
 }
-    
