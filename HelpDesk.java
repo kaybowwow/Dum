a95572c92b65750d62357983d395fbeee00dc3c1
kaybowwow <kbao@stuy.edu>
2017-04-23 19:51:07 -0400

    added runtimes, edited README

--- HelpDesk.java
+++ HelpDesk.java
@@ -14,62 +14,62 @@ public class HelpDesk{
       precond: 
       postcond: constructs a HelpDesk with an empty APQ of tickets and an empty AL of services
       ---------------------------------------*/        
-    public HelpDesk (){
+    public HelpDesk (){//O(1)
 	_tickets = new ArrayPriorityQueue<Ticket>();
 	_services = new ArrayList();
 	addServices();
-    }
+    }//end HelpDesk
 
     /*---------------------------------------
-      precond: a HelpDesk is created
+      precond: _services is created
       postcond: adds various services to _services
       ---------------------------------------*/        
-    public void addServices() {
+    public void addServices() { //O(1)
 	_services.add("dispatchTech");
 	_services.add("passReset");
 	_services.add("slowNetwork");
 	_services.add("coffee");
-    }
+    }//end addServices
     	
     /*---------------------------------------
       precond: int num
       postcond: returns a service at index num of _services
       if num is invalid, return the last service
       ---------------------------------------*/        
-    public static String assignDesc(int num) {
+    public static String assignDesc(int num) {//O(1)
 	if (num < _services.size()) {
 	    return _services.get(num);
 	}
 	System.out.println("Invalid selection. Default service chosen . . .");
 	return _services.get(_services.size()-1);
-    }	
+    }//end assignDesc	
 
     /*---------------------------------------
       precond: a HelpDesk is created
       postcond: prints tickets
       ---------------------------------------*/        
-    public void printTickets(){
+    public void printTickets(){//O(n)
 	System.out.println(_tickets);
-    }
+    }//end printTickets
 
     /*---------------------------------------
       precond: a HelpDesk is created
       postcond: prints services
       ---------------------------------------*/        
-    public void printServices() {
+    public void printServices() {//O(n)
 	String retStr = "";
 	for (int x = 0 ; x < _services.size() ; x ++) {
 	    retStr += "\n" + x + ": " + _services.get(x);
 	}
 	System.out.println(retStr);
-    }
+    }//end printServices
 
 
     /*---------------------------------------
       precond: HelpDesk is created
       postcond: adds a ticket to ArrayPriorityQueue _tickets based on priority of ticket
       ---------------------------------------*/        
-    public void addTicket( Ticket x ){
+    public void addTicket( Ticket x ){//O(n)
 	
 	//if empty, just add x at end of AL
 	if (_tickets.isEmpty() || _tickets.get(_tickets.size()-1).getPriority() <= x.getPriority() ) {
@@ -85,14 +85,14 @@ public class HelpDesk{
 		} // if
 	    } // for
 	}// else
-    }// add
+    }// addTicket
     
    /*---------------------------------------
       precond: HelpDesk is created
       postcond: dequeues and returns ticket with highest priority
       if there are no tickets, return null.
       ---------------------------------------*/        
-    public Ticket dequeue (){
+    public Ticket dequeue (){//O(n)
 	if (_tickets.isEmpty()) {
 	    System.out.println("There are no tickets pending.");
 	    return null;
@@ -105,14 +105,14 @@ public class HelpDesk{
 	
 	    return retTicket;
 	}
-    }
+    }//end dequeue
 
     /*---------------------------------------
       Requests user input and creates a new ticket based on user input
       precond: a HelpDesk is created
       postcond: returns a new ticket 
       ---------------------------------------*/    
-    public Ticket newRequest() {
+    public Ticket newRequest() {//O(n)
 	    
 	//user inputs name
 	System.out.println("Greetings. Please tell us your name.");
@@ -126,10 +126,10 @@ public class HelpDesk{
 	
 	Ticket a = new Ticket (inputName, inputDesc, serviceNum);
 	return a;
-    }
+    }//end newRequest
        	    
 
-    
+    //main
     public static void main (String[] args){
 	HelpDesk hDesk = new HelpDesk();
 
@@ -146,17 +146,13 @@ public class HelpDesk{
 	    System.out.println("Would you like to request a service? (select 0 or 1) \n0: yes\n1: no");
 	    input = Keyboard.readInt();
 	    if (input == 0) {
+		
 		ticketA = hDesk.newRequest();
+		
 		System.out.println("Your ticket has been added to the queue.\n");
 		hDesk.addTicket(ticketA);
-		
 		//hDesk.printTickets();
 		
-		System.out.println(hDesk.dequeue());
-		System.out.println(hDesk.dequeue());
-		System.out.println(hDesk.dequeue());
-		System.out.println(hDesk.dequeue());
-		System.out.println(hDesk.dequeue());
 		System.out.println(hDesk.dequeue());
 		System.out.println(hDesk.dequeue());
 		
@@ -164,5 +160,5 @@ public class HelpDesk{
 	    }
 	}
 	System.out.println("Goodbye!");
-    }
-}
+    }//end main
+}//end class HelpDesk
--- README.md
+++ README.md
@@ -3,18 +3,26 @@
 
 ## Design -
 
-1) Upon running the program, we prompt the user using a for loop for whether they need any help. This way they can get help / create multiple tickets. 
+We used an ArrayPriorityQueue to store tickets so that higher-priority tickets can be dequeued before lower-priority tickets.
 
-2) Users are then told to write their name if they said they needed help. Otherwise the program stops. 
+We used an ArrayList to store services in order or priority, so that a high-priority service will have a low index, and a low-priority service will have a high index. 
 
-3) Users place their problem within a service. We then use this to assign priority.
+1) Upon running the program, we prompt the user (using a for loop) for whether or not they need help. This way he/she can make multiple ticket requests.  
 
-4) Users wait their turn for a solution and step 1 is repeated.
+2) If the user needs help, then the user is told to input his/her name. Otherwise, the program exits the for loop, resolves, and stops. 
+
+3) The user then inputs his/her selection of a service. We then use this input to assign priority. If the user selects an invalid service, then the lowest-priority service will be automatically selected.
+
+4) All of the previously user-inputted information is then used to create and add a ticket that includes the user's name and service to a ticket list.
+
+5) The user who owns the ticket with the highest priority is granted his/her service.
+
+6) Step 1 is repeated.
 
 
 ## Extras
 1) The ticket's toString() allows the ticket to be displayed by the terminal as an actual ticket with the user's name, description, ticket number, and priority.
 
-2) We make the user choose which kind of problem they have, making prioritizing easier.
+2) We make the user choose from a list of services which kind of problem they have, making prioritizing easier.
 
-3) Added non-user tickets to the arrayPriorityQueue to simulate an actual help desk center.
+3) Added non-user created tickets to the ArrayPriorityQueue of tickets to simulate an actual help desk center.
